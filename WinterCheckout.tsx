5939ea5370a3b8ff17808eef3003af2a6a8d67f4
Evan Fisher <57010682+Evan-Fisher@users.noreply.github.com>
2022-10-06 09:22:58 -0700

    added in color shceme for next.js

--- WinterCheckout.tsx
+++ WinterCheckout.tsx
@@ -90,7 +90,7 @@ const WinterCheckout: React.FC<{
   return showModal ? (
     <div
       dangerouslySetInnerHTML={{
-        __html: `<iframe id="winter-checkout" src="${projectUrl}" style="position: fixed; top: 0px; bottom: 0px; right: 0px; width: 100%; border: none; margin: 0px; padding: 0px; overflow: hidden; z-index: 999999; height: 100%;" />`,
+        __html: `<iframe id="winter-checkout" src="${projectUrl}" style="color-scheme: light; position: fixed; top: 0px; bottom: 0px; right: 0px; width: 100%; border: none; margin: 0px; padding: 0px; overflow: hidden; z-index: 999999; height: 100%;" />`,
       }}
     />
   ) : (
